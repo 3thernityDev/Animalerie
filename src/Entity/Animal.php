0dc5f6d67b36501f899b4f5f5a59f6018929956a
3thernitydev <3thernitydev@gmail.com>
2024-11-20 10:59:46 +0400

    fix animal entity

---
+++ src/Entity/Animal.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnimalRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AnimalRepository::class)]
+class Animal
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $sex = null;
+
+    #[ORM\Column]
+    private ?float $birthday = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSex(): ?string
+    {
+        return $this->sex;
+    }
+
+    public function setSex(string $sex): static
+    {
+        $this->sex = $sex;
+
+        return $this;
+    }
+
+    public function getBirthday(): ?float
+    {
+        return $this->birthday;
+    }
+
+    public function setBirthday(float $birthday): static
+    {
+        $this->birthday = $birthday;
+
+        return $this;
+    }
+}
