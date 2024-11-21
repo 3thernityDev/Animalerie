2ad9bac45d91ae34eb223abb15269529b163335b
3thernitydev <3thernitydev@gmail.com>
2024-11-21 09:19:22 +0400

    add creating product form

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Product;
 use DateTime;
 use DateTimeImmutable;
+use App\Entity\Product;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProduitController extends AbstractController
 {
@@ -24,18 +27,27 @@ class ProduitController extends AbstractController
     #[Route('/createProduit', name: 'app_produit_create')]
     public function create(EntityManagerInterface $em): Response
     {
-        $croquette = new Product();
-        $croquette->setName('Croquette');
-        $croquette->setPrix(200);
-        $croquette->setDescription("Une croquette qui en fera bavé vos petit compagnons !");
-        $croquette->setCreateAt(new DateTimeImmutable());
-        $croquette->setActive(true);
+        // $croquette = new Product();
+        // $croquette->setName('Croquette');
+        // $croquette->setPrix(200);
+        // $croquette->setDescription("Une croquette qui en fera bavé vos petit compagnons !");
+        // $croquette->setCreateAt(new DateTimeImmutable());
+        // $croquette->setActive(true);
 
-        $em->persist($croquette); // Rend les données persistantes
-        $em->flush(); // Envoie à la BDD
+        // $em->persist($croquette); // Rend les données persistantes
+        // $em->flush(); // Envoie à la BDD
+
+        $produit = new Product();
+        $form = $this->createFormBuilder($produit)
+            ->add("name", TextType::class)
+            ->add("prix", NumberType::class)
+            ->add("Envoyer", SubmitType::class)
+
+            ->getForm();
 
         return $this->render('produit/index.html.twig', [
             'controller_name' => 'ProduitController',
+            "form" => $form
         ]);
     }
 }
