2731c0ebf27c444b5a22c51f79ee14da32291c29
3thernitydev <3thernitydev@gmail.com>
2024-11-21 10:03:41 +0400

    add products list

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -6,26 +6,41 @@ use DateTime;
 use DateTimeImmutable;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManager;
+use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProduitController extends AbstractController
 {
-    #[Route('/produit', name: 'app_produit')]
-    public function index(): Response
+    private ProductRepository $productRepository;
+
+    // Injection du repository dans le constructeur
+    public function __construct(ProductRepository $productRepository)
     {
+        $this->productRepository = $productRepository;
+    }
+
+
+    #[Route('/produits', name: 'app_produit')]
+    public function searchAll(): Response
+    {
+        $products = $this->productRepository->findAll();
+
         return $this->render('produit/index.html.twig', [
-            'controller_name' => 'ProduitController',
+            'controller_name' => 'AnimalerieController',
+            'products' => $products,
         ]);
     }
 
     #[Route('/createProduit', name: 'app_produit_create')]
-    public function create(EntityManagerInterface $em): Response
+    public function create(Request $request, EntityManagerInterface $em): Response
     {
         // $croquette = new Product();
         // $croquette->setName('Croquette');
@@ -39,11 +54,28 @@ class ProduitController extends AbstractController
 
         $produit = new Product();
         $form = $this->createFormBuilder($produit)
-            ->add("name", TextType::class)
-            ->add("prix", NumberType::class)
+            ->add("name", TextType::class, [
+                'label' => 'Nom du produit'
+            ])
+            ->add("description", TextareaType::class, [
+                'label' => 'Description'
+            ])
+            ->add("prix", NumberType::class, [
+                'label' => 'Prix'
+            ])
             ->add("Envoyer", SubmitType::class)
 
             ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $produit->setCreateAt(new DateTimeImmutable());
+            $produit->setActive(true);
+            $productData = $form->getData();
+            $em->persist($productData);
+            $em->flush();
+        }
+
 
         return $this->render('produit/index.html.twig', [
             'controller_name' => 'ProduitController',
